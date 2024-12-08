ad604203dd7be1d36d891437875587cb30990da1
Thomas aussenac <thomauss@gmail.com>
2024-12-08 18:06:07 +0100

    Config ini finito

--- app/IOT/src/main/java/projet/application/view/ConfigAlertViewController.java
+++ app/IOT/src/main/java/projet/application/view/ConfigAlertViewController.java
@@ -19,16 +19,17 @@ public class ConfigAlertViewController {
 
     private ProjetIOT App;
     @FXML
-    private TextField seuil_temperature, seuil_co2, seuil_humidity, seuil_power, seuil_energy;
+    private TextField seuil_temperature, seuil_co2, seuil_humidity, seuil_power, seuil_energy, frequence;
 
     @FXML
     private void doSauvegarder() throws IOException {
         try{
-            this.ecrivain.setSeuilTemp(seuil_temperature.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_temperature.getText()) : Integer.MAX_VALUE);
-            this.ecrivain.setSeuilCo2(seuil_co2.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_co2.getText()) : Integer.MAX_VALUE);
-            this.ecrivain.setSeuilHumdity(seuil_humidity.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_humidity.getText()) : Integer.MAX_VALUE);
-            this.ecrivain.setSeuilPower(seuil_power.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_power.getText()) : Integer.MAX_VALUE);
-            this.ecrivain.setSeuilEnergy(seuil_energy.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_energy.getText()) : Integer.MAX_VALUE);
+            this.ecrivain.setSeuilTemp(seuil_temperature.getText().length()>0 && seuil_temperature.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_temperature.getText()) : Integer.MAX_VALUE);
+            this.ecrivain.setSeuilCo2(seuil_co2.getText().length()>0 && seuil_co2.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_co2.getText()) : Integer.MAX_VALUE);
+            this.ecrivain.setSeuilHumdity(seuil_humidity.getText().length()>0 && seuil_humidity.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_humidity.getText()) : Integer.MAX_VALUE);
+            this.ecrivain.setSeuilPower(seuil_power.getText().length()>0 && seuil_power.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_power.getText()) : Integer.MAX_VALUE);
+            this.ecrivain.setSeuilEnergy(seuil_energy.getText().length()>0 && seuil_energy.getText().matches("-?[0-9]*") ? Integer.parseInt(seuil_energy.getText()) : Integer.MAX_VALUE);
+            this.ecrivain.setSeuilEnergy(frequence.getText().length()>0 && frequence.getText().matches("[0-9]*") ? Integer.parseInt(frequence.getText()) : 60);
 
             //On construit le fichier config.ini
             this.ecrivain.writeConfig();
